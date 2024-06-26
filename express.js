caaea248f068bb6c267fe2870c2a686dd9400b6c
rushikeshwayal <rushikes.wayal.ai@ghrcem.raisoni.net>
2024-06-27 02:31:26 +0530

    node commit dynamic app

---
+++ express.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+
+// express app
+const app = express();
+
+//register view engine
+app.set('view engine','ejs')
+
+//listen to express
+
+app.listen(3000);   
+
+app.get('/' , (req,res)=>{
+    const blogs = [
+        {title:'Ram wants apple', snippet :'lorem adeir nerirt'},
+        {title:'Good things around', snippet :'cozy corner café, a young woman'},
+        {title:'The day wore on', snippet :'n a world where magic was thought'},
+    ]
+res.render('newIndex',{title:'Home',blogs})
+
+})
+
+app.get('/about' , (req,res)=>{
+    res.render('about',{title:'About'});
+})
+
+app.get('/blogs/create' , (req,res)=>{
+    res.render('create',{title:'Create Blog'});
+}) 
+
+//redirect
+// app.get('./about-us',(req,res)=>{
+//     res.redirect('/about.html')
+// })
+
+// 404
+app.use((req,res)=>{
+    res.status(404).render('404',{title:'Error'})
+})
